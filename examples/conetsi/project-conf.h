35fdccd97b8cf5d1248f730e53f34f3eaa5bfcb1
Srijith Nair <srijithnair@iisc.ac.in>
2019-04-05 17:25:21 +0530

    Configure CSMA_MGMT flag in project-conf

--- examples/conetsi/project-conf.h
+++ examples/conetsi/project-conf.h
@@ -5,13 +5,13 @@
 /* #define LOG_CONF_LEVEL_RPL LOG_LEVEL_DBG */
 #define LOG_LEVEL_OAM LOG_LEVEL_DBG
 #define LOG_LEVEL_CONETSI LOG_LEVEL_DBG
+#define LOG_LEVEL_CSMA_MGMT_QLEN LOG_LEVEL_DBG
 
 #define LOG_CONF_LEVEL_6LOWPAN LOG_LEVEL_NONE
 /*---------------------------------------------------------------------------*/
+#define CONF_CSMA_MGMT 1
 #define CONF_PRIORITY PRIORITY_LINEAR
-/*---------------------------------------------------------------------------*/
 #define UIP_CONF_UIP_DS6_NOTIFICATIONS 1
-/*---------------------------------------------------------------------------*/
 /* #define LINKADDR_CONF_SIZE 8 */
 /*---------------------------------------------------------------------------*/
 #endif /* PROJECT_CONF_H_ */
