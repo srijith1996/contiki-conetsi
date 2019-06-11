57aa669fba98448c339091f8bd9d7ebfde703734
Aadreesh <aadreeshsahu@gmail.com>
2019-06-11 17:28:07 +0530

    Add original code file and change QueueSimulation.py

--- examples/conetsi/test-sims/old.py
+++ examples/conetsi/test-sims/old.py
@@ -1,19 +1,17 @@
 import numpy as np
-import Queue
+import queue
 import copy
 import matplotlib.pyplot as plt
-import datetime as dt
 
 # Input Parameters
-total_time =1  #float(input("Enter time for simulation (Hours): "))
-IAT_rate = 60 #float(input("Enter Job Arrival Rate (/Hour): "))
-ST_rate = 50  #float(input("Enter Job Service Rate (/Hour): "))
+total_time = int(input("Enter time for simulation (Hours): "))
+IAT_rate = int(input("Enter Job Arrival Rate (/Hour): "))
+ST_rate = int(input("Enter Job Service Rate (/Hour): "))
 rho = IAT_rate/ST_rate
 
-np.random.seed(int(dt.datetime.now().strftime('%s')))
 
 # Initialize Parameters
-qu = Queue.Queue()
+qu = queue.Queue()
 curr_process = None
 IAT = []
 ST = []
@@ -23,31 +21,22 @@ server_busy = False
 list_wait = []
 list_delay = []
 
-num_processes = int(np.random.poisson(IAT_rate) * total_time)
-print num_processes
+num_processes = int(np.random.poisson(IAT_rate)* total_time)
 num_processes_served = 0
 
 # Populate Inter-Arrival-Times (IAT)
 for i in range(num_processes):
-    #print "IAT iter: %d" % i
-    temp = np.random.exponential(1.0/IAT_rate)*60*60
-    #print temp
+    temp = np.random.exponential(1/IAT_rate)*60*60
     if i==0:
         IAT.append(0)
     else:
         IAT.append(int(temp - temp%1))
         
-print IAT
-
 # Populate Service-Times (ST) (where ST[i]!=0)
 while not len(ST) == num_processes:
-    #print len(ST), num_processes
-    temp = np.random.exponential(1.0/ST_rate)*60*60
-    #print temp, int(temp - temp%1)
+    temp = np.random.exponential(1/ST_rate)*60*60
     if not int(temp- temp%1)<1:
         ST.append(int(temp - temp%1))
-
-print ST
 
 # Save a copy of ST
 ST_copy = copy.deepcopy(ST)
@@ -55,26 +44,18 @@ ST_copy = copy.deepcopy(ST)
 # Get Arrival-Times (AT) from IAT starting at t=0
 # and initialize Waiting-Times to 0
 for i in range(num_processes):
-    #print "AT iter: %d" % i
     if i == 0:
         AT.append(0)    
     else:
         AT.append(AT[i-1] + IAT[i])
     wait_time.append(0)
 
-print AT
-
 # Simulation of M/M/1 Queue (i represents current time)
 
-qlen = []
-for i in range(int(total_time*60*60)):    
-    #print i
+for i in range(total_time*60*60):    
     if server_busy:
-        #if len(list(qu.queue)) != 0:
-        #  print list(qu.queue)
         for item in list(qu.queue):
             wait_time[item] = wait_time[item] + 1
-            print item, wait_time[item]
         ST[curr_process] = ST[curr_process] - 1
         if ST[curr_process] == 0:
             server_busy = False
@@ -82,7 +63,6 @@ for i in range(int(total_time*60*60)):
 
     for j in range(num_processes):
         if i== AT[j]:
-            print "Time for %d" %j
             qu.put(j)
 
     if not server_busy and not qu.empty():
@@ -101,24 +81,17 @@ for i in range(int(total_time*60*60)):
         list_wait.append(0)
         list_delay.append(0)
     else:
-        list_wait.append(sum_wait/(num_processes_served*60*60.0))  
-        list_delay.append(sum_delay/(num_processes_served*60*60.0))
+        list_wait.append(sum_wait/(num_processes_served*60*60))  
+        list_delay.append(sum_delay/(num_processes_served*60*60))
 
-    qlen.append(len(qu.queue))
-
-print list_wait, list_delay
-
-plt.plot([i+1 for i in range(int(total_time*60*60))], list_wait)
+plt.plot([i+1 for i in range(total_time*60*60)], list_wait)
 plt.ylabel("Avg Wait Times")
 plt.show()
 
-plt.plot([i+1 for i in range(int(total_time*60*60))], list_delay)
+plt.plot([i+1 for i in range(total_time*60*60)], list_delay)
 plt.ylabel("Avg Delay Times")
 plt.show()
 
-plt.plot([i+1 for i in range(int(total_time*60*60))], qlen)
-plt.ylabel("Queue length")
-plt.show()
 
 
 
