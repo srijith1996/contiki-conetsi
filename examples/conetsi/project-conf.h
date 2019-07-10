a2beb33d40d04c7f880bebb7147958ddf2759be6
Aadreesh <aadreeshsahu@gmail.com>
2019-07-10 11:39:50 +0530

    Error in running the whole simulation using udp only

--- examples/conetsi/project-conf.h
+++ examples/conetsi/project-conf.h
@@ -7,7 +7,7 @@
 #define LOG_LEVEL_CONETSI LOG_LEVEL_INFO
 #define LOG_LEVEL_DUMMY LOG_LEVEL_INFO
 #define LOG_LEVEL_QSIM_MOD LOG_LEVEL_DBG
-
+#define LOG_LEVEL_NORMAL LOG_LEVEL_INFO
 #define LOG_LEVEL_CSMA_MGMT_QLEN LOG_LEVEL_DBG
 #define LOG_LEVEL_CSMA_MGMT_RTX LOG_LEVEL_DBG
 
@@ -16,6 +16,9 @@
 #define CONF_DUMMY     0
 #define CONF_QSIM_MODULE 1    
 #define CONF_CSMA_MGMT 0
+/*---------------------------------------------------------------------------*/
+#define CONF_CONETSI   0
+#define CONF_NORMAL    1
 /*---------------------------------------------------------------------------*/
 #define CONF_PRIORITY PRIORITY_LINEAR
 #define UIP_CONF_UIP_DS6_NOTIFICATIONS 1
